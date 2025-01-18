7b7986e1d8382c3040f66c5a23ed82621b48532c
unknown <setan.apolline@gmail.com>
2025-01-18 16:25:00 +0100

    added true logo

--- src/Components/LeftMenu.jsx
+++ src/Components/LeftMenu.jsx
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
 import "../Styles/LeftMenu.css";
 import { SiApplemusic } from "react-icons/si";
+import logoImage from "../assets/logoImage.png";
+
+// Components
 import { Menu } from "./Menu";
 import { MenuList } from "./MenuList";
 
 function LeftMenu() {
-  // Utilisez un état pour suivre l'élément de menu actif
+  // Création d'un état pour suivre l'élément de menu actif
   const [activeIndex, setActiveIndex] = useState(null);
 
   // Fonction pour gérer le clic sur un élément de menu
@@ -17,9 +20,8 @@ function LeftMenu() {
     <div className="leftMenu">
       <div className="logoContainer">
         <i>
-          <SiApplemusic />
+          <img src={logoImage} alt="Logo" className="logoImage" />
         </i>
-        <h2>Let's Jam</h2>
       </div>
 
       {/* Passer activeIndex et handleMenuClick aux deux menus */}
