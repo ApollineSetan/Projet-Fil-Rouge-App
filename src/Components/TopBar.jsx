9ce6e56204f51c18774050833dbbd2c621d51c6e
ApollineSetan <setan.apolline@gmail.com>
2025-02-09 13:25:37 +0100

    handlereturnclick added

--- src/Components/TopBar.jsx
+++ src/Components/TopBar.jsx
@@ -1,12 +1,20 @@
 import React from "react";
+import {useNavigate} from "react-router-dom";
 import "../Styles/TopBar.css";
 import { BiSearchAlt, BiSolidBell } from "react-icons/bi";
 import { IoIosArrowBack } from "react-icons/io";
 
 function TopBar() {
+
+  const navigate = useNavigate();
+
+  const handleReturnClick = () => {
+    navigate(-1); // Cela va revenir à la page précédente dans l'historique
+  };
+
   return (
     <div className="containerTopBar">
-      <button className="returnButton">
+      <button className="returnButton" onClick={handleReturnClick}>
         <i className="return">
           <IoIosArrowBack />
         </i>
