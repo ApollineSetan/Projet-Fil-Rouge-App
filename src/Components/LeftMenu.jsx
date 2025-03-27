a209fc0081fb04b3bfdd990b7f38243f6689a9e8
unknown <setan.apolline@gmail.com>
2025-03-27 11:38:31 +0100

    reactrouter on profile

--- src/Components/LeftMenu.jsx
+++ src/Components/LeftMenu.jsx
@@ -1,17 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../Styles/LeftMenu.css";
 import logoImage from "../assets/logoImage.png";
 import { Menu } from "./Menu";
 import { MenuList } from "./MenuList";
+import { useLocation } from "react-router-dom"; // Importer le hook useLocation
 
 function LeftMenu() {
   // Création d'un état pour suivre l'élément de menu actif
   const [activeIndex, setActiveIndex] = useState(null);
 
-  // Fonction pour gérer le clic sur un élément de menu et mettre l'index actif
-  const handleMenuClick = (index) => {
-    setActiveIndex(index);
-  };
+  // Utilisation de useLocation pour obtenir l'URL actuelle
+  const location = useLocation();
+
+  useEffect(() => {
+    // Logique pour définir l'index de l'élément actif en fonction de l'URL actuelle
+    if (location.pathname === "/add-demo") {
+      setActiveIndex(2); // "Profil" (id: 2) est actif sur la page AddDemo
+    } else if (location.pathname === "/") {
+      setActiveIndex(2); // "Profil" (id: 2) est actif sur la page EditionPage
+    } else if (location.pathname.startsWith("/edit-demo")) {
+      setActiveIndex(2); // "Profil" (id: 2) est actif sur la page InfoDemo
+    } else {
+      setActiveIndex(null); // Si aucune condition ne correspond, aucun élément n'est surligné
+    }
+  }, [location]); // Mettre à jour l'index actif chaque fois que l'URL change
 
   return (
     <div className="leftMenu">
@@ -26,14 +38,14 @@ function LeftMenu() {
         menuObject={MenuList.slice(0, 6)}
         className="primaryMenu"
         activeIndex={activeIndex}
-        onMenuClick={handleMenuClick}
+        onMenuClick={setActiveIndex}
       />
 
       <Menu
         menuObject={MenuList.slice(6, 9)}
         className="secondaryMenu"
         activeIndex={activeIndex}
-        onMenuClick={handleMenuClick}
+        onMenuClick={setActiveIndex}
       />
     </div>
   );
